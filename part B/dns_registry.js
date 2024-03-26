f16215f0220fe04e1743c26f06c8d4aae4b8c5ed
alexiavinci <alexia.igue@edu.devinci.fr>
2024-03-26 22:20:55 +0100

    q6

--- part B/dns_registry.js
+++ part B/dns_registry.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3002; // Utiliser un port différent pour le registre DNS
 
 app.get('/getServer', (req, res) => {
-  res.json({code: 200, server: "localhost:3001"});
+  res.json({code: 200, server: "localhost:3000"});
 });
 
 app.listen(port, () => {
