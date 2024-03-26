4dfa39b295c7aabe6578fb0775cc36200325da1d
alexiavinci <alexia.igue@edu.devinci.fr>
2024-03-26 21:16:24 +0100

    part B hello world

---
+++ part B/dns_registry.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3002; // Utiliser un port différent pour le registre DNS
+
+app.get('/getServer', (req, res) => {
+  res.json({code: 200, server: "localhost:3001"});
+});
+
+app.listen(port, () => {
+  console.log(`DNS Registry running at http://localhost:${port}`);
+});
